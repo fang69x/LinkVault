f707cb9b93fb3270eca494ed4692edfa60a7b794
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-15 18:05:34 +0530

    User model finished

---
+++ src/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const userSchema= new mongoose.Schema({
+    name:{
+     type:String,
+     required:true,
+    },
+    email:{
+        type:String,
+     required:true,
+     unique:true,
+
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    avatar:{
+      public_id:String,
+      url:String
+    },
+
+},{
+    timestamps:true
+});
+
+export default mongoose.model("User",userSchema);

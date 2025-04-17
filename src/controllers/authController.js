34ac5a575d10ffa776615fda281809f0d9c94481
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-17 13:38:04 +0530

    login controller done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -72,3 +72,48 @@ return res.status(400).json({
     });
 }
 };
+
+
+
+// Login User
+export const loginUser=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        // find the user using email
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({
+message:"User not found , Please sign up",
+            });
+        }
+        // if user found then compare the password 
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message:"Invalid credentials",
+                            });
+        }
+        // generate JWT token if passwords match
+        const token=jwt.sign({
+            userId:user._id,},
+            process.env.JWT_SECRET,
+            {
+              expiresIn:"1h",
+
+            }
+        );
+        res.status(200).json({
+            message:"Login Successful",
+            token,
+             user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                avatar:user.avatar?.url||null
+             }
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+        res.status(500).json({ message: "Error logging in", error: error.message || error });
+    }
+}

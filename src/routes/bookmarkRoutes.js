89b1a0ed79fa769a8684f1c2d657c61518e88235
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-18 21:18:24 +0530

    sort confid error fixed

--- src/routes/bookmarkRoutes.js
+++ src/routes/bookmarkRoutes.js
@@ -3,14 +3,16 @@ import { createBookmark,
     getBookmarks, 
     getBookmarkById, 
     updateBookmark, 
-    deleteBookmark, } from "../controllers/bookmarkController.js";
+    deleteBookmark,searchBookmarks } from "../controllers/bookmarkController.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", authenticateUser, createBookmark);
 router.get("/", authenticateUser, getBookmarks);
+router.get("/search",authenticateUser, searchBookmarks);
 router.get("/:id", authenticateUser,getBookmarkById);
 router.put("/:id", authenticateUser,updateBookmark);
 router.delete("/:id", authenticateUser,deleteBookmark);
+
 export default router;

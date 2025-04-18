fdd537bfdb103dec3453dea03512bd104e33a874
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-18 20:21:06 +0530

    Basic crud working

--- src/controllers/bookmarkController.js
+++ src/controllers/bookmarkController.js
@@ -1,5 +1,17 @@
 import { Bookmark } from "../models/bookmark.model.js";
 
+//Handling specific errors
+const handleErrors = (error, res) => {
+    if (error.name === 'ValidationError') {
+      return res.status(400).json({ message: "Validation Error", error: error.message });
+    }
+    if (error.code === 11000) {
+      return res.status(409).json({ message: "Duplicate URL detected for this user" });
+    }
+    return res.status(500).json({ message: "Server Error", error: error.message });
+  };
+
+
 // create a new bookmark
 export const createBookmark=async(req,res)=>{
     try {
@@ -15,16 +27,75 @@ export const createBookmark=async(req,res)=>{
         const savedBookmark=await bookmark.save();
         res.status(201).json(savedBookmark)
     } catch (error) {
-        res.status(500).json({ message: "Error creating bookmark", error: error.message });
+        handleErrors(error,res);
     }
 };
+
 //get all bookmark for a user
 
 export const getBookmarks=async(req,res)=>{
     try {
-        const bookmarks=await Bookmark.find({user:req.user._id});
+        const bookmarks=await Bookmark.find({user:req.user._id}).sort({createdAt:-1});
         res.status(200).json(bookmarks);
     } catch (error) {
-        res.status(500).json({ message: "Error fetching bookmarks", error: error.message });
+        handleErrors(error,res);
+    }
+}
+
+// get single bookmark
+export const getBookmarkById=async(req,res)=>{
+    try {
+        const bookmark=await Bookmark.findOne({_id:req.params.id,
+            user:req.user.id
+        });
+        if (!bookmark) {
+            return res.status(404).json({ message: "Bookmark not found" });
+          }
+          res.status(200).json(bookmark);
+    } catch (error) {
+        handleErrors(error,res);
+    }
+}
+//update bookmark
+export const updateBookmark=async(req,res)=>{
+    try {
+       const {title,url,note,category,tags}=req.body;
+       //find and update
+       const updatedBookmark=await Bookmark.findByIdAndUpdate({
+        _id:req.params.id,
+        user:req.user.id,
+       },
+    {
+        title,url,note,category,tags
+    },
+    {
+        new:true
+    }
+    );
+    if(!updatedBookmark){
+        return res.status(404).json({ message: "Bookmark not found or unauthorized" });
+    }
+    res.status(200).json({
+        updateBookmark,
+    })
+    } catch (error) {
+        handleErrors(error,res);
+    }
+}
+//delete a bookmark
+export const deleteBookmark=async(req,res)=>{
+    try {
+        const deletedBookmark=await Bookmark.findByIdAndDelete({
+            _id:req.params.id,
+            user:req.user.id
+        });
+        if(!deletedBookmark){
+            return res.status(404).json({ message: "Bookmark not found or unauthorized" });
+        }
+        res.status(200).json({
+            message:"Bookmark deleted successfully"
+        });
+    } catch (error) {
+        handleErrors(error,res);
     }
 }
--- src/routes/bookmarkRoutes.js
+++ src/routes/bookmarkRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createBookmark, getBookmarks } from "../controllers/bookmarkController.js";
+import { createBookmark, 
+    getBookmarks, 
+    getBookmarkById, 
+    updateBookmark, 
+    deleteBookmark, } from "../controllers/bookmarkController.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", authenticateUser, createBookmark);
 router.get("/", authenticateUser, getBookmarks);
-
+router.get("/:id", authenticateUser,getBookmarkById);
+router.put("/:id", authenticateUser,updateBookmark);
+router.delete("/:id", authenticateUser,deleteBookmark);
 export default router;

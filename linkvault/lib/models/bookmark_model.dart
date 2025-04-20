790c2c2ee187b77fa8e80aa5347525af43831857
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-20 14:52:06 +0530

    bookmark model and user model

---
+++ linkvault/lib/models/bookmark_model.dart
@@ -0,0 +1,49 @@
+class Bookmark {
+  final String? id;
+  final String title;
+  final String url;
+  final String? note;
+  final String category;
+  final List<String> tags;
+  final String userId;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  Bookmark({
+    this.id,
+    required this.title,
+    required this.url,
+    this.note,
+    required this.category,
+    required this.tags,
+    required this.userId,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory Bookmark.fromJson(Map<String, dynamic> json) {
+    return Bookmark(
+      id: json['_id'],
+      title: json['title'],
+      url: json['url'],
+      note: json['note'],
+      category: json['category'],
+      tags: List<String>.from(json['tags'] ?? []),
+      userId: json['user'],
+      createdAt:
+          json['createdAt'] != null ? DateTime.parse(json['createdAt']) : null,
+      updatedAt:
+          json['updatedAt'] != null ? DateTime.parse(json['updatedAt']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'url': url,
+      'note': note,
+      'category': category,
+      'tags': tags,
+    };
+  }
+}

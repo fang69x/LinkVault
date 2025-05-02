9d63a1207aa7c60893ca3678d7a3f4be1e8e213f
fang69x <109065342+fang69x@users.noreply.github.com>
2025-05-02 23:23:03 +0530

    Deploy Flutter web build

--- linkvault/vercel-deploy/flutter_bootstrap.js
+++ linkvault/vercel-deploy/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "723960089"
+    serviceWorkerVersion: "1034025465"
   }
 });
